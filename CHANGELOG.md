7e97ff38abe8ded40d630ecc15bd7f1006751e1c
dblock <dblock@dblock.org>
2015-12-27 11:41:19 -0500

    Preparing for next development iteration, 0.1.1.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 0.1.1 (Next)
+
+* Your contribution here.
+
 ### 0.1.0 (12/27/2015)
 
 * Initial public release - [@dblock](https://github.com/dblock).
--- lib/newrelic-slack-ruby-bot/version.rb
+++ lib/newrelic-slack-ruby-bot/version.rb
@@ -2,7 +2,7 @@ module NewRelic
   module Agent
     module Instrumentation
       module SlackRubyBot
-        VERSION = '0.1.0'
+        VERSION = '0.1.1'
       end
     end
   end
